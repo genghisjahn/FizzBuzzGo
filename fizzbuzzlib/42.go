615515c6366824213981e7cf6f5a382d06e03754
genghisjahn <jon.m.wear@gmail.com>
2013-09-30 21:43:44 -0400

    Added an exclamation point.

--- fizzbuzzlib/42.go
+++ fizzbuzzlib/42.go
@@ -1,15 +1,15 @@
 package fizzbuzz
 
-import(
+import (
 	"strconv"
 )
 
-type FizzBuzzIs42 struct {}
+type FizzBuzzIs42 struct{}
 
-func(fb42 FizzBuzzIs42) GetResult(num int) string{
-	result:=strconv.Itoa(num)
-	if num==42{
-		result = "The ultimate answer!"
+func (fb42 FizzBuzzIs42) GetResult(num int) string {
+	result := strconv.Itoa(num)
+	if num == 42 {
+		result = "The ultimate answer!!"
 	}
 	return result
 }
