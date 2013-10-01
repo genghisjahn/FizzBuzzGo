9a7b8340eeee5e46d2dcaf29c65b3be3afe5c9dd
genghisjahn <jon.m.wear@gmail.com>
2013-09-30 21:46:47 -0400

    Added an exclamation point.

--- fizzbuzzlib/perfectsquare.go
+++ fizzbuzzlib/perfectsquare.go
@@ -1,17 +1,18 @@
 package fizzbuzz
 
-import(
-	"strconv"
+import (
 	"math"
+	"strconv"
 )
 
-type FizzBuzzPerfectSquare struct {}
-func(fbps FizzBuzzPerfectSquare) GetResult(num int) string{
-	result:=strconv.Itoa(num)
-	root:=math.Sqrt(float64(num))
-	introot:=int(root)
-	if root==float64(introot){
-		result = "Perfect Square"
+type FizzBuzzPerfectSquare struct{}
+
+func (fbps FizzBuzzPerfectSquare) GetResult(num int) string {
+	result := strconv.Itoa(num)
+	root := math.Sqrt(float64(num))
+	introot := int(root)
+	if root == float64(introot) {
+		result = "Perfect Square!"
 	}
 	return result
 }
