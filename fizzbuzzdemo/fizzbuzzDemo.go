207e337ce3d1d9413446e75d29917ee4f1edf0cc
genghisjahn <jon.m.wear@gmail.com>
2013-09-30 21:48:16 -0400

    Adding a comment.  Okay, the git is working correctly now.

--- fizzbuzzdemo/fizzbuzzDemo.go
+++ fizzbuzzdemo/fizzbuzzDemo.go
@@ -1,32 +1,27 @@
 package main
 
 import (
-	"fmt"
 	sfb "fizzbuzzgo/fizzbuzzlib"
-	
+	"fmt"
 )
 
 func main() {
-	fbp := sfb.FizzBuzzProcessor{GetItems()}	
+	fbp := sfb.FizzBuzzProcessor{GetItems()}
 	for i := 1; i <= 100; i++ {
-			result:=fbp.GetResult(i)
-			fmt.Printf("\n%v",result)
+		result := fbp.GetResult(i)
+		fmt.Printf("\n%v", result)
 	}
-	
-		
+	//Adding a comment.
+
 }
-func GetItems() sfb.Items{
-	fbpno:=sfb.FizzBuzzNumberOnly{}
+func GetItems() sfb.Items {
+	fbpno := sfb.FizzBuzzNumberOnly{}
 	var _ = fbpno
-	fbps:=sfb.FizzBuzzPerfectSquare{}
-	fbpalin:=sfb.FizzBuzzPalindrome{}
-	fb42:=sfb.FizzBuzzIs42{}
-	fbdiv15:=sfb.FizzBuzzDivisor{15,"FizzBuzz"}
-	fbdiv5:=sfb.FizzBuzzDivisor{5,"Buzz"}
-	fbdiv3:=sfb.FizzBuzzDivisor{3,"Fizz"}
-	return sfb.Items{fbps,fb42,fbpalin,fbdiv15,fbdiv5,fbdiv3,}
+	fbps := sfb.FizzBuzzPerfectSquare{}
+	fbpalin := sfb.FizzBuzzPalindrome{}
+	fb42 := sfb.FizzBuzzIs42{}
+	fbdiv15 := sfb.FizzBuzzDivisor{15, "FizzBuzz"}
+	fbdiv5 := sfb.FizzBuzzDivisor{5, "Buzz"}
+	fbdiv3 := sfb.FizzBuzzDivisor{3, "Fizz"}
+	return sfb.Items{fbps, fb42, fbpalin, fbdiv15, fbdiv5, fbdiv3}
 }
-
-
-
-
